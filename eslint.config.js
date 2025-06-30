96c7bf488ce42f8f5acd28672e34bca26ef514ad
mminseop <alstjq903@naver.com>
2025-06-30 13:36:11 +0900

    test Commmit

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
-import js from '@eslint/js'
 import globals from 'globals'
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -7,23 +8,41 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ['**/*.{js,jsx,cjs,mjs}'],
+  },
+  {
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      }
+    }
+  },
+  js.configs.recommended,
+  react.configs.flat.recommended,
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh
+    },
+    settings: {
+      react: {
+        version: 'detect',
       },
     },
     rules: {
+      ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-undef': 'error',
+      'eqeqeq': ['error', 'always'],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-alert': 'warn',
+      'no-var': 'error',
+      'no-multiple-empty-lines': 'warn',
+      'dot-notation': 'warn',
+      'react/react-in-jsx-scope': 'off'
     },
   },
 ])
