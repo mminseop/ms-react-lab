a27dd3e3588e83fcff1a1ebf5a44a9bc1d0ab464
mminseop <alstjq903@naver.com>
2025-06-30 16:48:24 +0900

    todo ui 수정 및 리스트까지

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Home from './pages/Home';
-import TodoList from './pages/TodoList';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import TodoList from "./pages/TodoList";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <div className="page-container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/todo" element={<TodoList />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <div className="page-container">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/todo" element={<TodoList />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+import "../styles/TodoList.css";
+
 function TodoList() {
-  return <h2>📝 This is the Todo List Page</h2>;
+    const [todo, setTodo] = useState([
+        { id: 0, content: "데이터1" },
+        { id: 1, content: "데이터2" },
+    ]);
+
+    const [inputValue, setInputValue] = useState('');
+
+    return (
+        <>
+            <div className="todo-container">
+                <TodoListHeader />
+                <TodoListItems todo={todo} />
+                <TodoInput todo={todo}/>
+            </div>
+        </>
+    );
 }
+
+function TodoListHeader() {
+    return <h1>📝 Todo List</h1>;
+}
+
+function TodoListItems({ todo }) {
+    return (
+        <div className="todo-list">
+            {todo.map((x) => {
+                return (
+                    <div key={x.id} className="todo-item">
+                        {x.content}
+                        <div className="btn-wrap">
+                            <button>수정</button>
+                            <button>삭제</button>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+function TodoInput({todo}) {
+    return (
+        <div className="input-section">
+            <input
+                type="text"
+                placeholder="할 일을 입력하세요"
+                value = {inputValue}
+                onChange={(e) => {setInputValue(e.target.value)}}
+            />
+            <button>추가</button>
+        </div>
+    );
+}
+
 
 export default TodoList;
